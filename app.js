fa68bd90300d96768e2ee26df5ce2bb9aa5e2dae
Gisela-Camacho-H <gis25cam@gmail.com>
2021-12-02 19:47:25 -0600

    change links in the views

--- app.js
+++ app.js
@@ -13,3 +13,9 @@ app.listen(process.env.PORT || port, () => console.log(`Servidor corriendo en pu
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
